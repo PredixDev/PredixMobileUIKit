502b21c8723b5d93850f2230f5e1b7016bec3272
George Fouche <fouliex@gmail.com>
2017-12-01 17:05:53 -0800

    loadAndStackChart has less parameters

--- PredixMobileUIKit/PredixBarChartView.swift
+++ PredixMobileUIKit/PredixBarChartView.swift
@@ -10,18 +10,6 @@ import Charts
 import Foundation
 import PredixSDK
 
-class BarChartBuilder {
-    
-    var x: Double?
-    var y: Double?
-    var z: Double?
-    
-    typealias BuilderClosure = (BarChartBuilder) -> ()
-    
-    init(buildClosure: BuilderClosure) {
-        buildClosure(self)
-    }
-}
 open class PredixBarChartView: BarChartView {
     /// Array of colors to use. Defaults to UIColor.Predix.DataVisualizationSets.regular
     open var dataVisualizationColors: [UIColor] = UIColor.Predix.DataVisualizationSets.regular
@@ -30,13 +18,13 @@ open class PredixBarChartView: BarChartView {
         super.init(frame: frame)
         initialize()
     }
-    
+
     /// :nodoc:
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initialize()
     }
-    
+
     /// Predix Mobile Bar chart initial values
     fileprivate func initialize() {
         legend.horizontalAlignment = .right
@@ -44,12 +32,12 @@ open class PredixBarChartView: BarChartView {
         xAxis.labelPosition = .bottom
         chartDescription?.enabled = false
     }
-    
+
     /// Set message if there's not data provided
     public func setNoDataText(message: String) {
         noDataText = message
     }
-    
+
     /// Add a limit line
     /// - parameter limit: Double number of where the line should be drawn verticaly
     /// - parameter label: String label of the line
@@ -57,40 +45,40 @@ open class PredixBarChartView: BarChartView {
         let ll = ChartLimitLine(limit: limit, label: label)
         rightAxis.addLimitLine(ll)
     }
-    
+
     /// Set the x axis label text color
     /// - parameter uiColor: provide a color base on the class UIColor
     public func setXAxisLabelTextColor(uiColor: UIColor) {
         xAxis.labelTextColor = uiColor
     }
-    
+
     /// Helper function to populate the bar chart based on one data entry
     /// - parameter xValues: String array of x values
     /// - parameter yValues: Double array of y values
     /// - parameter label: String label
     /// - parameter showWithDefaultAnimation: optional parameter to show the chart with the default animation. Defaults to `true`. If `false` the caller is responsible for calling one of the `animate` methods to provide custom display animation.
     public func loadChart(xValues: [String], yValues: [Double], label: String, showWithDefaultAnimation: Bool = true) {
-        
+
         var dataEntries: [BarChartDataEntry] = []
-        
+
         for i in 0 ..< yValues.count {
             let dataEntry = BarChartDataEntry(x: Double(i), y: yValues[i])
             dataEntries.append(dataEntry)
         }
-        
+
         let chartDataSet = BarChartDataSet(values: dataEntries, label: label)
         chartDataSet.colors = dataVisualizationColors
-        
+
         let chartData = BarChartData(dataSet: chartDataSet)
         data = chartData
-        
+
         xAxis.valueFormatter = IndexAxisValueFormatter(values: xValues)
-        
+
         if showWithDefaultAnimation {
             animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
         }
     }
-    
+
     /// Helper function to populate the bar chart based on  two data entries
     /// - parameter xValues: String array of x values
     /// - parameter yValues1: Double array of the first data entry
@@ -100,37 +88,36 @@ open class PredixBarChartView: BarChartView {
     /// - parameter uiColor1: provide a color base on the class UIColor for the first data entry
     /// - parameter uiColor2: provide a color base on the class UIColor for the second data entry
     /// - parameter showWithDefaultAnimation: optional parameter to show the chart with the default animation. Defaults to `true`. If `false` the caller is responsible for calling one of the `animate` methods to provide custom display animation.
-    public func loadAndStackChart(xValues: [String], yValues1: [Double], yValues2: [Double], label1: String, label2: String, uiColor1AnduiColor2: ([UIColor],[UIColor]), showWithDefaultAnimation: Bool = true) {
-        
+    public func loadAndStackChart(xValues: [String], yValues1AndyValue2: ([Double], [Double]), label1AndLabel2: (String, String), uiColor1AnduiColor2: ([UIColor], [UIColor]), showWithDefaultAnimation: Bool = true) {
+
         var dataEntries: [BarChartDataEntry] = []
         var dataEntries2: [BarChartDataEntry] = []
-        
+
+        let (yValues1, yValues2) = yValues1AndyValue2
         for i in 0 ..< yValues1.count {
-            // let dataEntry = BarChartDataEntry(x: Double(i), y: yValues[i])
             let dataEntry = BarChartDataEntry(x: Double(i), y: yValues1[i])
             dataEntries.append(dataEntry)
         }
-        
+
         for i in 0 ..< yValues2.count {
             let dataEntry = BarChartDataEntry(x: Double(i), y: yValues2[i])
             dataEntries2.append(dataEntry)
         }
-        
+
         // chartDataSet = BarChartDataSet(values: dataEntries, label: label)
+        let (label1, label2) = label1AndLabel2
         let chartDataSet = BarChartDataSet(values: dataEntries, label: label1)
         let chartDataSet1 = BarChartDataSet(values: dataEntries2, label: label2)
-        
+
         (chartDataSet.colors, chartDataSet1.colors) = uiColor1AnduiColor2
-      
-        
+
         let dataSets: [BarChartDataSet] = [chartDataSet, chartDataSet1]
         data = BarChartData(dataSets: dataSets)
-        
+
         xAxis.valueFormatter = IndexAxisValueFormatter(values: xValues)
-        
+
         if showWithDefaultAnimation {
             animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
         }
     }
 }
-
--- PredixMobileUIKitDemo/PredixMobileUIKitDemo/BarChartDemoViewController.swift
+++ PredixMobileUIKitDemo/PredixMobileUIKitDemo/BarChartDemoViewController.swift
@@ -11,36 +11,35 @@ import PredixMobileUIKit
 import UIKit
 
 class BarchartDemoViewController: UIViewController {
-    
-    
-    @IBOutlet weak var barChartView: PredixBarChartView!
+
+    @IBOutlet var barChartView: PredixBarChartView!
     var months: [String]!
     override func viewDidLoad() {
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         let unitsBought = [10.0, 2.0, 20.0, 4.0, 5, 8.0, 9.0, 15.0, 1.0, 3.0, 10.0, 18.0]
-        
+
         // Load and Stack the Bar Chart with two data entry
-        barChartView.loadAndStackChart(xValues: months, yValues1: unitsSold, yValues2: unitsBought, label1: "Units Sold", label2: "Units Bought", uiColor1AnduiColor2: ([NSUIColor.red],[NSUIColor.blue]), showWithDefaultAnimation: false)
-        
+        barChartView.loadAndStackChart(xValues: months, yValues1AndyValue2: (unitsSold, unitsBought), label1AndLabel2: ("Units Sold", "Units Bought"), uiColor1AnduiColor2: ([NSUIColor.red], [NSUIColor.blue]), showWithDefaultAnimation: false)
+
         // Load the Bar Char with one data entry
         // barChartView.loadChart(xValues: months, yValues: unitsSold, label: "Units Sold",showWithDefaultAnimation : false)
-        
+
         // Add a limit line
         barChartView.addALimit(limit: 10.0, label: "Target")
-        
+
         /// By default the legend horizontal alignment is set to right and the vertical alignment is set to top.
         /// Below is an example of  how the legend can be position differently
         barChartView.legend.horizontalAlignment = .left
         barChartView.legend.verticalAlignment = .top
-        
+
         // To enable or disable the legend
         barChartView.legend.enabled = true
-        
+
         // By default the x axis label text color is black.
         // Below is an example on how to set the x axis label text color
         barChartView.setXAxisLabelTextColor(uiColor: NSUIColor.red)
-        
+
         // By default tje x axis label position is set to bottom
         // Below is an example o how to change the x xais position
         barChartView.xAxis.labelPosition = .bottom
