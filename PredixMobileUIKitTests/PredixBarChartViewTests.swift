b7b8524d65ccb52fcfcc634381f9bb2e77d817a6
George Fouche <fouliex@gmail.com>
2017-12-07 16:40:23 -0800

    format

--- PredixMobileUIKitTests/PredixBarChartViewTests.swift
+++ PredixMobileUIKitTests/PredixBarChartViewTests.swift
@@ -31,11 +31,11 @@ class PredixBarChartViewTests: XCTestCase {
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         let unitsBought = [10.0, 2.0, 20.0, 4.0, 5, 8.0, 9.0, 15.0, 1.0, 3.0, 10.0, 18.0]
-        
-        let bar1 = Bar(unitsSold, label: "Units Sold",color:[NSUIColor.blue])
-        let bar2 = Bar(unitsBought, label: "Units Bought",color:[NSUIColor.red])
-        
-        barChart.create(xAxisValues: months, bars: [bar1,bar2],showWithDefaultAnimation : true)
+
+        let bar1 = Bar(unitsSold, label: "Units Sold", color: [NSUIColor.blue])
+        let bar2 = Bar(unitsBought, label: "Units Bought", color: [NSUIColor.red])
+
+        barChart.create(xAxisValues: months, bars: [bar1, bar2], showWithDefaultAnimation: true)
         XCTAssertTrue(barChart.data?.dataSetCount ?? 0 > 0, "No datasets were loaded in prepareForInterfaceBuilder")
         waitForExpectations(timeout: 1, handler: nil)
     }
@@ -52,10 +52,10 @@ class PredixBarChartViewTests: XCTestCase {
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         let unitsBought = [10.0, 2.0, 20.0, 4.0, 5, 8.0, 9.0, 15.0, 1.0, 3.0, 10.0, 18.0]
-        let barChart1 = Bar(unitsSold, label: "Units Sold",color:[NSUIColor.blue])
-        let barChart2 = Bar(unitsBought, label: "Units Bought",color:[NSUIColor.red])
-        
-        barChart.create(xAxisValues: months, bars: [barChart1,barChart2],showWithDefaultAnimation : true)
+        let barChart1 = Bar(unitsSold, label: "Units Sold", color: [NSUIColor.blue])
+        let barChart2 = Bar(unitsBought, label: "Units Bought", color: [NSUIColor.red])
+
+        barChart.create(xAxisValues: months, bars: [barChart1, barChart2], showWithDefaultAnimation: true)
         XCTAssertTrue(barChart.data?.dataSetCount ?? 0 > 0, "No datasets were loaded in prepareForInterfaceBuilder")
         waitForExpectations(timeout: 1, handler: nil)
     }
